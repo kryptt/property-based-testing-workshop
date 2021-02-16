8d0db9769ce67fac2357d5899b06dec0e4cd74c6
Rodolfo Hansen <rhansen@kitsd.com>
2021-02-16 20:22:34 +0100

    Add Session workflow

--- 02/main.cc
+++ 02/main.cc
@@ -1,16 +1,47 @@
 #include <rapidcheck.h>
 
-#include <vector>
-#include <algorithm>
+using namespace rc;
 
-int main() {
-  rc::check("double reversal yields the original value",
-            [](const std::vector<int> &l0) {
-              auto l1 = l0;
-              std::reverse(begin(l1), end(l1));
-              std::reverse(begin(l1), end(l1));
-              RC_ASSERT(l0 == l1);
-            });
+enum class Gender
+{
+  Male,
+  Female
+};
+
+std::ostream &operator<<(std::ostream &os, Gender gender)
+{
+  os << ((gender == Gender::Male) ? "Male" : "Female");
+  return os;
+}
+
+struct User
+{
+  std::string username;
+  Gender gender;
+};
+
+namespace rc
+{
+
+  template <>
+  struct Arbitrary<User>
+  {
+    static Gen<User> arbitrary()
+    {
+      return gen::build<User>(
+          gen::set(&User::username),
+          gen::set(&User::gender, gen::element(Gender::Male, Gender::Female)));
+    }
+  };
+
+} // namespace rc
+
+int main()
+{
+  rc::check("RC_TAG", [](const User &user) { RC_TAG(user.gender); });
+
+  rc::check("RC_CLASSIFY",
+            [](const User &user) { RC_CLASSIFY(user.username.empty()); });
 
   return 0;
 }
--- README.md
+++ README.md
@@ -13,3 +13,18 @@ Build docker image.
 Open docker container.
 
   ./run.sh
+
+## Planned Session
+
+* Intro into Property Based Testing
+* Look at example 01 - understand how the idea is to note invariant properties being confirmed
+* Exercise 01 - write a function that checks for the min of two doubles and identity the post condition which is invariant.
+* Look at example 02 - understand generators
+* Exercise 02 - write a class to represent a Sensor measurement device, give it a name, 4 supported IMAGE resolution modes with
+different read frequencies for each. Write an invariant property that confirms no invalid frequency is used for a given resolution
+and use RC_TAG to notice the distribution of combination checks.
+* Look at example 03 - understand state
+* Exercise 03 - extend the Sensor class with an internal Opened / Closed state, and has the following commands: open/close/readImage
+write your own property based test that is aware of the Sensor state and validates the sequence of commands sent to the sensor and
+how it is expected to react.
+* Wrap up - future additional ideas
