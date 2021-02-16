d83c96e04163bc3fa871c81bca1307ebcb84dade
Rodolfo Hansen <rhansen@kitsd.com>
2021-02-16 17:22:47 +0100

    use internal repo

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 # pull "rapidcheck" submodule and its submodules (catch, googletest)
 git submodule update --init --recursive 01/ext/rapidcheck/
-docker build -t wsrc:latest .
+docker build -t dl-innersource-docker-local.artifactory-ehv.ta.philips.com/swcoe/pbt-workshop:0.1.0  .
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker run -it --rm --entrypoint /bin/bash \
   -v $PWD/01:/workshop_rapidcheck/01 \
   -v $PWD/02:/workshop_rapidcheck/02 \
   -v $PWD/03:/workshop_rapidcheck/03 \
-  -w /workshop_rapidcheck wsrc:latest
+  -w /workshop_rapidcheck dl-innersource-docker-local.artifactory-ehv.ta.philips.com/swcoe/pbt-workshop:0.1.0
